69215a55fb2e0f79fb0c61f34f344c914bd13904
Matthew Blackburn <79994779+matthewblackbu@users.noreply.github.com>
2025-03-20 10:55:57 -0400

    Update Base.vue

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="baseBeverage"></div>
+  <div class="baseBeverage" :style="{ backgroundColor: currentBase.color }"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { currentBase } from '../stores/beverage';
+</script>
 
 <style scoped>
 .baseBeverage {
@@ -12,6 +14,5 @@
   bottom: 0;
   animation: pour-tea 2s;
   z-index: 300;
-  /* // border-radius: 0.05em 0.05em 2.2em 2.2em; */
 }
 </style>
